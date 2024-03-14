02790807102904f14c5e79c49d8066732f530be5
abdul <abdulrahman12072002@gmail.com>
2024-03-14 10:59:56 +0530

    14-03-24

--- Function10.java
+++ Function10.java
@@ -3,8 +3,16 @@
 import java.util.*;
 public class Function10 {
     public static void CalculatePower(int x, int y){
-        int power = (int) Math.pow(x,y);
-        System.out.println(power);
+        // int power = (int) Math.pow(x,y);
+        // System.out.println(power);
+
+        int z = x;
+        
+        for(int i = 1; i < y; i++){
+           z=z*x;
+        }
+        System.out.println(z);
+
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
